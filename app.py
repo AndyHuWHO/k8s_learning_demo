35e9ec7ca7b2da73c851bf555c2277dc31b15c2d
AndyHuGenerac <Andy.Hu@generac.com>
2023-05-11 11:34:44 -0700

    minor bugs fixed, no data in database error

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ page_template = '''
         '''
 
 app = Flask(__name__)
-cache = redis.StrictRedis(host='cache', port=6379)
+# cache = redis.StrictRedis(host='cache-service', port=6379)
+cache = redis.StrictRedis(host='cache-service.demo.svc.cluster.local', port=6379)
 
 @app.route('/')
 def root():
@@ -31,8 +32,8 @@ def root():
 
 def db_get_teams():
     conn = mariadb.connect(
-        host="db",
-        database="acorn",
+        host="db-service.demo.svc.cluster.local",
+        database="demo-db",
         user=os.environ['MARIA_USER'],
         password=os.environ['MARIA_PASS'],
     )
