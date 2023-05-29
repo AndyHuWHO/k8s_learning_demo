0b765575e63771d5a5d07777654d5e649a21f1ad
AndyHuGenerac <Andy.Hu@generac.com>
2023-05-29 10:28:28 -0700

    customized webpage content

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template_string
 page_template = '''
         <div style="margin: auto; text-align: center;">
         <h1>{{ welcome_text }}</h1><br>
-        You're visitor #{{ visitors }} to learn about the FIFA 2022 World Cup Semifinalists:<br>
+        You're visitor #{{ visitors }} to view Andy Hu's Top 5 NBA GOAT players:<br>
         <ul>
             {%- for team in teams %}
             <li>{{ team }}</li>
@@ -19,7 +19,7 @@ page_template = '''
 
 app = Flask(__name__)
 # cache = redis.StrictRedis(host='cache-service', port=6379)
-cache = redis.StrictRedis(host='cache-service.demo.svc.cluster.local', port=6379)
+cache = redis.StrictRedis(host='cache-service.andy-demo.svc.cluster.local', port=6379)
 
 @app.route('/')
 def root():
@@ -39,7 +39,8 @@ def db_get_teams():
     )
 
     cur = conn.cursor()
-    cur.execute("SELECT teams FROM semifinalists;")
+    # cur.execute("SELECT teams FROM semifinalists;")
+    cur.execute("SELECT player FROM goatplayers;")
 
     return [x[0] for x in cur.fetchall()] 
 
